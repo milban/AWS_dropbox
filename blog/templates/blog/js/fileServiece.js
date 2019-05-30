dff48b5a82d8c8268d84227ed9039506a9673dd4
junyoki <wnsgur6311@gmail.com>
2019-05-30 21:44:10 +0900

    when user click submit, Request to server and todo

--- blog/templates/blog/js/fileServiece.js
+++ blog/templates/blog/js/fileServiece.js
@@ -1,3 +1,4 @@
+const xhr = new XMLHttpRequest()
 var content = document.querySelector('.content')
 //var testBtn = document.querySelector('.testBtn')
 var currentPath // 파일이름 뺀 현재 경로
@@ -79,9 +80,26 @@ btn.addEventListener('change', btnChangeEventHandler)
 
 // 유저가 전송버튼 클릭 시
 var form = document.querySelector('.file-form')
- form.onsubmit = function() {
-     
- 
+form.onsubmit = function() {
+    const filePathObj = { filePath: currentFilePath }
+    const jsonFileObj = JSON.stringify(filePathObj)
+    const url =""
+    
+    xhr.open('post', url) // 비동기 방식으로 Request 오픈
+    xhr.send(jsonFileObj) // Request 전송
+
+    xhr.onreadystatechange = function(e) {
+        if(xhr.status==200) {
+            console.log(xhr.responseText)
+            xhr.onprogress = function(evt) {
+                var progressBar = document.querySelector('#progressBar')
+                progressBar.value = evt.loaded/evt.total*100;
+            }
+        } else {
+            console.log("xhr response error")
+        }
+    }
+
     return false //중요! false를 리턴해야 버튼으로 인한 submit이 안된다.
  }
 
