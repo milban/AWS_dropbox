856574a25b18007574f3fb9cb3a0eaaaf72f23e6
junyoki <wnsgur6311@gmail.com>
2019-06-07 14:25:01 +0900

    to test make fileUpload bucket put code to comment

--- blog/views.py
+++ blog/views.py
@@ -133,7 +133,7 @@ class Main_View(View):
         elif request.POST.get("request") == "file_upload":
             file_name = request.POST.get("file_name")
             path = request.POST.get("curPath")
-            self.bucket_put_file(file_name)
+            #self.bucket_put_file(file_name)
             self.file_save(path + file_name)  # ex > KhuKhuBox/file.txt
             context = {'status': "ok"}
             return HttpResponse(json.dumps(context), content_type="application/json")
