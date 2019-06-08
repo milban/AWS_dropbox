b823c15b93c75d9124409dd880f7eb88b7dd0b16
junyoki <wnsgur6311@gmail.com>
2019-06-08 17:59:45 +0900

    to test cookie

--- blog/static/js/login.js
+++ blog/static/js/login.js
@@ -1,14 +1,15 @@
-var form = document.querySelector('.userInfo')
-form.onsubmit = function() {
+// var form = document.querySelector('.userInfo')
+// form.onsubmit = function() {
+//     var userId = document.querySelector('#userid').textContent
+//     console.log(userId)
+//     document.cookie = "userId="+userId
+//     return true;
+// }
+
+var btn = document.querySelector('.submit-btn')
+function submitBtnHandler() {
     var userId = document.querySelector('#userid').textContent
     console.log(userId)
     document.cookie = "userId="+userId
-    return true;
 }
-
-// var btn = document.querySelector('.submit-btn')
-// function submitBtnHandler() {
-//     var userId = document.querySelector('#userid')
-//     console.log(userId)
-// }
-// btn.addEventListener('click', submitBtnHandler)
+btn.addEventListener('click', submitBtnHandler)
