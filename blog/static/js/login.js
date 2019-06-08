cf8a43e074213b819f958965f8097a83413afdf1
junyoki <wnsgur6311@gmail.com>
2019-06-08 17:57:27 +0900

    to test cookie

--- blog/static/js/login.js
+++ blog/static/js/login.js
@@ -1,6 +1,6 @@
 var form = document.querySelector('.userInfo')
 form.onsubmit = function() {
-    var userId = document.querySelector('#userid')
+    var userId = document.querySelector('#userid').textContent
     console.log(userId)
     document.cookie = "userId="+userId
     return true;
