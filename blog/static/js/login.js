0c7c503fda87248bcc6ed078d5789b94daf976e6
BEAN <nogadahalf12@naver.com>
2019-06-12 21:39:38 +0900

    login test

--- blog/static/js/login.js
+++ blog/static/js/login.js
@@ -7,25 +7,18 @@
 // }
 
 
-function redirectToMain(url) {
+function redirectToMain() {
     const xhr = new XMLHttpRequest()
-    var formdata = new FormData();
-    formdata.append("request", "create_directory")
-    formdata.append("user_id", getCookie('userId'))
-    formdata.append("curPath", currentPath + dirname + '/')
     const url =""
-
-    console.log("user_id" + ':' + getCookie('userId'))
-    console.log("curPath: " + currentPath + dirname + '/') 
     
-    xhr.open('POST', url) // 비동기 방식으로 Request 오픈
+    xhr.open('GET', url) // 비동기 방식으로 Request 오픈
+    xhr.setRequestHeader("AUTHORIZATION", getCookie('userId'))
     xhr.onreadystatechange = function() {
         if(xhr.status==200) {
             console.log(xhr.responseText)
             if(xhr.readyState==4) {
                 console.log(xhr.response)
                 //postContentsOfDirAndPrint(currentPath)
-                window.location.href("이동할 곳 넣자")
                 //리다이렉트
             }
         } else {
@@ -51,12 +44,11 @@ function submitBtnHandler() {
     setCookie('userId', userId, 1)
     //쿠키 설정
     //이 아래로는 토큰 받은걸로 처리하자
-    /*
+    
     const xhr = new XMLHttpRequest()
     var formdata = new FormData();
-    formdata.append("request", "create_directory")
-    formdata.append("user_id", getCookie('userId'))
-    formdata.append("curPath", currentPath + dirname + '/')
+    formdata.append("userId", getCookie('userId'))
+    formdata.append("password", document.getElementById('userpw'))
     const url =""
 
     console.log("user_id" + ':' + getCookie('userId'))
@@ -69,9 +61,11 @@ function submitBtnHandler() {
             if(xhr.readyState==4) {
                 console.log(xhr.response)
                 //postContentsOfDirAndPrint(currentPath)
-                var temptoken = ""
-                if(temptoken!=undefined)
-                redirectToMain("url 넣자")
+                var strFileList = xhr.response
+                var token = JSON.parse(strFileList)['token']
+                if(token!=null)
+                setCookie('jwt', toekn, 1)
+                redirectToMain()
             }
         } else {
             console.log("xhr response error")
@@ -79,7 +73,7 @@ function submitBtnHandler() {
         }
     }
     xhr.send(formdata) // Request 전송
-    */
+    
 }
 
 btn.addEventListener('click', submitBtnHandler)
